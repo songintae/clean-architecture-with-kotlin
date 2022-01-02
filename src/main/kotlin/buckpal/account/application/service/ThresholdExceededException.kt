2014b5dfba71544425e98c13a6821fa1fc4f8c05
songintae <songintae92@hanmail.net>
2022-01-02 22:59:48 +0900

    chapter03: ThresholdExceededException 생성 방식을 변경하라

--- src/main/kotlin/buckpal/account/application/service/ThresholdExceededException.kt
+++ src/main/kotlin/buckpal/account/application/service/ThresholdExceededException.kt
@@ -2,7 +2,18 @@ package buckpal.account.application.service
 
 import buckpal.account.domain.Money
 
-class ThresholdExceededException(
-    threshold: Money,
-    actual: Money,
-) : RuntimeException("Maximum threshold for transferring money exceeded: tried to transfer $actual but threshold is $threshold")
+class ThresholdExceededException : RuntimeException {
+    constructor(
+        message: String
+    ) : super(message)
+
+    constructor(
+        message: String,
+        cause: Throwable
+    ) : super(message = message, cause = cause)
+
+    constructor(
+        threshold: Money,
+        actual: Money
+    ) : super("Maximum threshold for transferring money exceeded: tried to transfer $actual but threshold is $threshold")
+}
